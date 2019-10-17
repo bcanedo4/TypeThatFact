260937dfefa764f7d3af3f877faeb2f7709535dd
Brian Canedo <brian.canedo.600@my.csun.edu>
2019-10-17 13:51:45 -0700

    Script knows when user input equals fact

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const h1 = document.createElement('h1');
 const headingContainer = document.createElement('div');
 const textareaContainer = document.createElement('div');
 const textarea = document.createElement('textarea');
+const count = document.createElement('p');
 
 // fetch random fact
 fetch(`https://uselessfacts.jsph.pl/random.json?language=en`)
@@ -10,8 +11,6 @@ fetch(`https://uselessfacts.jsph.pl/random.json?language=en`)
     return response.json()
   })
   .then(data => {
-    
-
     // add classes and data for fact and container
     headingContainer.setAttribute('class', 'heading__container')
     h1.textContent = data.text;
@@ -23,23 +22,42 @@ fetch(`https://uselessfacts.jsph.pl/random.json?language=en`)
     textarea.setAttribute('class', 'textarea');
     textarea.setAttribute('id', 'textareaId');
 
+    // add classes and data for count
+    count.setAttribute('class', 'character-count');
+
     app.appendChild(headingContainer);
     headingContainer.appendChild(h1);
     app.appendChild(textareaContainer);
     textareaContainer.appendChild(textarea);
+    textarea.appendChild(count);
   })
   .catch(err => {
     console.log(err);
   })
 
-// let textarea = document.getElementById('textarea');
+const compareText = (text, length) => {
+  let fact = h1.innerHTML;
+  // let factCount = h1.innerHTML.length;
+  // console.log(factCount);
+  let factCompare = fact.substring(0, length);
+  if (factCompare === text) {
+    console.log('right');
+  } else {
+    console.log('wrong');
+  }
+
+  console.log(text, length);
+}
+
 if (textarea.addEventListener) {
   textarea.addEventListener('input', () => {
     // Chrome, Firefox
-    console.log(textarea.value);
+    // count.innerHTML = ""
+
+    compareText(textarea.value, textarea.value.length);
   }, false);
 } else if (textarea.attachEvent) {
-    console.log('ree', textarea);
+    console.log('IE', textarea.value);
 }
 
 console.log(textarea);
